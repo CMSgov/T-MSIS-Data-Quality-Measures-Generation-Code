d3aeb05c24e9f3ff10b0727914db03b9c566b4b9
V2I8Mathematica <164403359+V2I8Mathematica@users.noreply.github.com>
2025-08-18 11:04:24 -0400

    V.4.0.1 updates (#40)
    * V.4.0.1 updates

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open(".github/README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dqm",
-    version="3.13.0",
+    version="4.00.1",
     author="Shinu Verghese",
     author_email="shinu.verghese@cms.hhs.gov",
     description="A package to calculate data quality measures on T-MSIS data using Databricks",
